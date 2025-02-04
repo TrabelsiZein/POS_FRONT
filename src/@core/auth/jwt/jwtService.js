e340fdae17fa75b0038015c5733bd4a66bd23252
Zein Elabidine TRABELSI <zat@wevioo.com>
2025-02-04 13:19:12 +0100

    222

--- src/@core/auth/jwt/jwtService.js
+++ src/@core/auth/jwt/jwtService.js
@@ -27,7 +27,8 @@ export default class JwtService {
         // If token is present add it to request's Authorization Header
         if (accessToken) {
           // eslint-disable-next-line no-param-reassign
-          config.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`
+          config.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`;
+          config.headers["X-Company-ID"] = 1
         }
         return config
       },
